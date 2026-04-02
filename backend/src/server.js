cfaef3b748e45b779d312e98fec4e44913982218
vishalumare <vishaldemo2004@gmail.com>
2026-04-02 12:54:22 +0530

    Codebase setup done, Setup for the deployment

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,16 +1,28 @@
-// const express = require("express");
 import express from "express"
-import 'dotenv/config'
+import 'dotenv/config';
+import path from "path";
+
 import authRouter from "../routes/auth.route.js";
 import messagesRoute from "../routes/messages.route.js";
 
 const app = express();
+const __dirname = path.resolve();
 
 const PORT = process.env.PORT ;
 
+// Routes
 app.use("/api/auth", authRouter);
-
 app.use("/api/messages", messagesRoute);
+
+
+// make ready for deployment 
+if(process.env.NODE_ENV === "production"){
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+  app.get("*", (req, res)=> {
+    res.sendFile(path.join(__dirname, "../frontend/dist/index.html"));
+  })
+}
 
 app.listen(PORT, () => {
   console.log(`Server is running on port: ${PORT}`);
